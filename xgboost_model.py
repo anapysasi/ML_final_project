c5a46565baa587d8ec7b6465498ec792301caf96
anapysasi <aysasi@ucm.es>
2021-05-30 22:24:18 -0500

    xgboost models

--- xgboost_model.py
+++ xgboost_model.py
@@ -9,7 +9,7 @@ warnings.filterwarnings('ignore')
 
 x_train, y_train, x_test, y_test = train_generator_func()
 print(x_train.shape[0], 'train samples')
-print(x_test.shape[0], 'test samples')
+print(x_test.shape[0], 'test samples \n')
 
 # Create XGB Classifier object
 xgb_clf = xgb.XGBClassifier(objective="multi:softmax")
