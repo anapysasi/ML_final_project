69481624948d7c033be39ba75d8043cf9d22d0a2
anapysasi <57515498+anapysasi@users.noreply.github.com>
2021-05-30 20:52:55 -0500

    Update example_model.py

--- example_model.py
+++ example_model.py
@@ -1,35 +1,74 @@
+"""
+This script assigns our train and test data to variables
+"""
+
+import os
+import glob
 import tensorflow as tf
 import numpy as np
-from train_test_split import train_generator_func
-from train_test_split import test_label_func
-from sklearn.metrics import accuracy_score
+import matplotlib.pyplot as plt
+import re
 
-train_generator = train_generator_func(info=True)
-files, test_label, test_label_fruit = test_label_func()
 
-image_classifier = tf.keras.Sequential()
-image_classifier.add(tf.keras.layers.Conv2D(filters=32, kernel_size=(3, 3),
-                                            input_shape=train_generator.image_shape, activation='relu'))
-image_classifier.add(tf.keras.layers.MaxPooling2D(pool_size=(2, 2)))
-image_classifier.add(tf.keras.layers.Conv2D(filters=64, kernel_size=(3, 3), activation='relu'))
-image_classifier.add(tf.keras.layers.MaxPooling2D(pool_size=(2, 2)))
-image_classifier.add(tf.keras.layers.Flatten())
-image_classifier.add(tf.keras.layers.Dense(units=128, activation='relu'))
-image_classifier.add(tf.keras.layers.Dense(units=4, activation='softmax'))
-image_classifier.compile(optimizer='Adam', loss='categorical_crossentropy', metrics=['accuracy'])
+def train_generator_func(info=False, image=False, shear_range_val=0.2, 
+                         zoom_range_val=0.2, target_size1=68, target_size2=46):
+    """
 
-image_classifier.fit(train_generator, steps_per_epoch=3, epochs=3)
+    :param info: If True prints information about the generator.
+    :param image: If True shows some of the images with the corresponding label
+    :param shear_range_val: value for shear_range in ImageDataGenerator()
+    :param zoom_range_val: value for zoom_range in ImageDataGenerator()
+    :param target_size1: first value of the tuple for target_size in ImageDataGenerator()
+    :param target_size2: second value of the tuple for target_size in ImageDataGenerator()
+    :return: train data generator
+    """
 
-data = []
-results = []
-for f1 in files:
-    img = tf.keras.preprocessing.image.load_img(f1, target_size=train_generator.image_shape)
-    img = tf.keras.preprocessing.image.img_to_array(img)
-    img = np.expand_dims(img, axis=0)
-    data.append(img)
-    result = image_classifier.predict(img)
-    r = np.argmax(result, axis=1)
-    results.append(r)
+    train_data_generator = tf.keras.preprocessing.image.ImageDataGenerator(rescale=1./255, shear_range=shear_range_val,
+                                                                           zoom_range=zoom_range_val, 
+                                                                           horizontal_flip=True)
+    train_generator = train_data_generator.flow_from_directory('data/Train', target_size=(target_size1, target_size2),
+                                                               batch_size=32, class_mode='categorical')
+    if info:
+        print('The image shape of each training observation is:', train_generator.image_shape)
+        print('\n We can see how:')
+        for i in range(len(train_generator.class_indices)):
+            print(list(train_generator.class_indices.keys())[i],
+                  'is labeled as', list(train_generator.class_indices.values())[i])
 
-accuracy = accuracy_score(results, test_label)
-print('\n\nThe accuracy score is:', accuracy)
+    if image:
+        fig, axs = plt.subplots(2, 2, figsize=(10, 10))
+        for i, ax in enumerate(axs.flatten()):
+            img, label = train_generator.next()
+            _ = ax.set_title(f'Label = {np.argmax(label[i])}')
+            _ = ax.imshow(img[i])
+        plt.show()
+    return train_generator
+
+
+def test_label_func():
+    """
+    :return: returns a vector with the labels of the test files
+    """
+    data_path = os.path.join('data/Test', '*g')
+    files = glob.glob(data_path)
+
+    test_label_fruit = list()
+    test_label = list()
+    label = None
+    for f in files:
+        photo = re.sub(r"data/Test/", "", f)
+        photo = re.findall(r"[a-zA-Z]", photo)
+        photo = ''.join(photo)
+        photo = re.sub(r"png", "", photo)
+        if photo == 'Apple':
+            label = 0
+        if photo == 'Banana':
+            label = 1
+        if photo == 'Orange':
+            label = 2
+        if photo == 'Tamotoes':
+            photo = 'Tomato'
+            label = 3
+        test_label_fruit.append(photo)
+        test_label.append(label)
+    return files, test_label, test_label_fruit
