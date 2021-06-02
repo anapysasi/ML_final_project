b4e25cfadb842bce67f8af726b17df2cf164930c
anapysasi <aysasi@ucm.es>
2021-06-02 10:24:35 -0500

    xgboost image augmentation changes

--- image_augmentation.py
+++ image_augmentation.py
@@ -5,14 +5,17 @@ import tensorflow as tf
 import glob
 warnings.filterwarnings('ignore')
 
-plot = False
+plot = True
+save_img = False
+it = None
 items = ['Apple', 'Banana', 'Orange', 'Tomato']
 
 for fruit in items:
     train_path = glob.glob('data/Train/' + fruit + '/*')
     augm_path = 'data/Augmentation/' + fruit + '/'
 
-    for f in train_path[0:int(0.2 * len(train_path))]:
+    # for f in train_path[0:int(0.2 * len(train_path))]:
+    for f in train_path[3000:3004]:
         datagen = tf.keras.preprocessing.image.ImageDataGenerator(width_shift_range=[-50, 50],
                                                                   height_shift_range=0.4, rotation_range=90,
                                                                   brightness_range=[0.2, 1.0],
@@ -21,14 +24,17 @@ for fruit in items:
         img = tf.keras.preprocessing.image.load_img(f)
         data = tf.keras.preprocessing.image.img_to_array(img)
         samples = np.expand_dims(data, 0)
-        it = datagen.flow(samples, batch_size=1,
-                          save_to_dir=augm_path,
-                          save_prefix=fruit + '_augm',
-                          save_format="png")
+        if save_img:
+            it = datagen.flow(samples, batch_size=1,
+                              save_to_dir=augm_path,
+                              save_prefix=fruit + '_augm',
+                              save_format="png")
+        else:
+            it = datagen.flow(samples)
         batch = it.next()
         image = batch[0].astype('uint8')
 
-if plot is True:
+if plot:
     for i in range(9):
         plt.subplot(330 + 1 + i)
         batch = it.next()
