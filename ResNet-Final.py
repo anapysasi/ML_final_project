35765d2fd6c339a65937d01287922d767c3b2f94
cryptoenthusiast93 <sachdevsa@gmail.com>
2021-06-02 13:08:57 -0500

    Update ResNet-Final.py

--- ResNet-Final.py
+++ ResNet-Final.py
@@ -163,14 +163,14 @@ base_model.save('ResNet50Base.h5')
 print("Saved model")
 
 
-# In[51]:
+# In[155]:
 
 
 model = load_model("ResNet50Base.h5")
 print('\n Model download successfully')
 
 
-# In[21]:
+# In[156]:
 
 
 from sklearn.metrics import accuracy_score
@@ -184,7 +184,7 @@ val_acc_values = history_dict['val_acc']
 epochs = range(1, len(history_dict['acc']) + 1)
 
 
-# In[22]:
+# In[157]:
 
 
 # Plot the training/validation loss
@@ -197,7 +197,7 @@ plt.legend()
 plt.show()
 
 
-# In[23]:
+# In[158]:
 
 
 # Plot the training/validation accuracy
@@ -210,7 +210,7 @@ plt.legend()
 plt.show()
 
 
-# In[26]:
+# In[159]:
 
 
 # Evaluate the test accuracy and test loss of the model
@@ -227,7 +227,7 @@ from keras.applications.resnet50 import preprocess_input
 from keras import applications
 
 
-# In[87]:
+# In[149]:
 
 
 # Test Data Directory and Looping Over Images
@@ -251,10 +251,30 @@ for f1 in files:
 print(results)
 
 
-# In[ ]:
+# In[160]:
 
 
+true_classes = validation_generator.classes
+class_indices = train_generator.class_indices
+class_indices = dict((v,k) for k,v in class_indices.items())
 
+resnet_preds = base_model.predict(validation_generator)
+resnet_pred_classes = np.argmax(resnet_preds, axis=1)
+
+resnet_acc = accuracy_score(true_classes, resnet_pred_classes)
+print("ResNet50 Model Accuracy with Fine-Tuning: {:.2f}%".format(resnet_acc * 100))
+
+print(true_classes)
+print(resnet_pred_classes)
+
+accuracy = accuracy_score(true_classes, resnet_pred_classes)
+print('\n\nThe accuracy score is:', accuracy)
+
+# Summarize the fit of the model on the test data
+print('\n Classification report of the test data: \n')
+print(metrics.classification_report(true_classes, resnet_pred_classes))
+print('\n Confusion matrix of the test data: \n')
+print(metrics.confusion_matrix(true_classes, resnet_pred_classes, normalize='true').round(3))
 
 
 # In[ ]:
@@ -335,7 +355,7 @@ def train_generator_func(img_size1=224, img_size_2=224):
     return x_train, y_train, x_test, y_test
 
 
-# In[54]:
+# In[138]:
 
 
 from tensorflow.keras.applications import ResNet50
@@ -352,40 +372,33 @@ base2_model.add(ResNet50(include_top=False, weights=None, pooling='max'))
 base2_model.add(Dense(4, activation='sigmoid'))
 
 
-# In[55]:
+# In[139]:
 
 
 base2_model.compile(optimizer = tf.keras.optimizers.SGD(lr=0.0001), loss = 'binary_crossentropy', metrics = ['acc'])
 
 
-# In[57]:
+# In[140]:
 
 
 resnet_history2 = base2_model.fit(train_generator, validation_data = validation_generator, steps_per_epoch = 50, epochs = 10)
 
 
-# In[58]:
+# In[141]:
 
 
-base2_model.save('ResNet50-Try2.h5')
+base2_model.save('ResNet50-Try2-2.h5')
 print("Saved model")
 
 
-# In[96]:
+# In[ ]:
 
 
-# save model to file
-pickle.dump(base2_model, open('base2_model.pickle.dat', 'wb'))
-
-
-# In[98]:
-
-
-resnet_history2 = load_model('ResNet50-Try2.h5')
+resnet_history2 = load_model('ResNet50-Try2-2.h5')
 print("Loaded model")
 
 
-# In[99]:
+# In[143]:
 
 
 from sklearn.metrics import accuracy_score
@@ -399,7 +412,7 @@ val_acc_values2 = history_dict2['val_acc']
 epochs2 = range(1, len(history_dict2['acc']) + 1)
 
 
-# In[100]:
+# In[144]:
 
 
 # Plot the training/validation loss
@@ -412,7 +425,7 @@ plt.legend()
 plt.show()
 
 
-# In[101]:
+# In[145]:
 
 
 # Plot the training/validation accuracy
@@ -425,7 +438,7 @@ plt.legend()
 plt.show()
 
 
-# In[102]:
+# In[146]:
 
 
 # Evaluate the test accuracy and test loss of the model
@@ -433,7 +446,7 @@ test_loss2, test_acc2 = base2_model.evaluate_generator(validation_generator)
 print('Model testing accuracy/testing loss:', test_acc2, " ", test_loss2)
 
 
-# In[ ]:
+# In[147]:
 
 
 # Test Data Directory and Looping Over Images
@@ -457,20 +470,108 @@ for f1 in files:
 print(results)
 
 
+# In[153]:
+
+
+true_classes = validation_generator.classes
+class_indices = train_generator.class_indices
+class_indices = dict((v,k) for k,v in class_indices.items())
+
+resnet_preds = base2_model.predict(validation_generator)
+resnet_pred_classes = np.argmax(resnet_preds, axis=1)
+
+resnet_acc = accuracy_score(true_classes, resnet_pred_classes)
+print("ResNet50 Model Accuracy without Fine-Tuning: {:.2f}%".format(resnet_acc * 100))
+
+print(true_classes)
+print(resnet_pred_classes)
+
+accuracy = accuracy_score(true_classes, resnet_pred_classes)
+print('\n\nThe accuracy score is:', accuracy)
+
+# Summarize the fit of the model on the test data
+print('\n Classification report of the test data: \n')
+print(metrics.classification_report(true_classes, resnet_pred_classes))
+print('\n Confusion matrix of the test data: \n')
+print(metrics.confusion_matrix(true_classes, resnet_pred_classes, normalize='true').round(3))
+
+
 # # Model 3 - Steps:100; Epochs:20 (None)
 
-# In[ ]:
+# In[106]:
 
 
 import pickle
 
 
-# In[ ]:
+# In[123]:
+
+
+def train_generator_func(img_size1=225, img_size_2=225):
+    """
+
+    :param img_size1: first value of the tuple to resize the image
+    :param img_size_2: second value of the tuple to resize the image
+    :param augmentation: default True. Adds the images form the augmentation process to teh data.
+    :param reshaped: default True. Reshapes the data
+    :return: x_train, y_train, x_test, y_test ready to use in xg_boost
+    """
+    train_path = glob.glob('data/Train/*/*')
+    test_path = glob.glob('data/Test/Groups/*/*')
+   
+    def inp_process(file):
+        data = []
+        _label = None
+        for f in file:
+            try:
+                part = f.split('/')
+                assert part[-2] in ['Apple', 'Banana', 'Orange', 'Tomato']
+                if part[-2] == 'Apple':
+                    _label = 0
+                elif part[-2] == 'Banana':
+                    _label = 1
+                elif part[-2] == 'Orange':
+                    _label = 2
+                elif part[-2] == 'Tomato':
+                    _label = 3
+                img = cv2.imread(f, cv2.IMREAD_GRAYSCALE)
+                r_siz = cv2.resize(img, (img_size1, img_size_2))
+            except Exception as e:
+                raise Exception(e)
+            data.append([r_siz, _label])
+        return np.array(data)
+
+    train = inp_process(train_path)
+    test = inp_process(test_path)
+
+    x_train = []
+    y_train = []
+    x_test = []
+    y_test = []
+
+    for feature, label in train:
+        x_train.append(feature)
+        y_train.append(label)
+
+    for feature, label in test:
+        x_test.append(feature)
+        y_test.append(label)
+
+    return x_train, y_train, x_test, y_test
+
+
+# In[124]:
+
+
+x_train, y_train, x_test, y_test = train_generator_func()
+
+
+# In[128]:
 
 
 from tensorflow.keras.applications import ResNet50
 from tensorflow.python.keras.models import Sequential
-from tensorflow.python.keras.layers import Dense, Flatten, GlobalAveragePooling2D, Dropout
+from tensorflow.python.keras.layers import Dense, Flatten, GlobalAveragePooling2D, Dropout, Activation
 
 base3_model = ResNet50(input_shape=(224, 224,3), include_top=False, weights=None)
 
@@ -480,29 +581,36 @@ for layer in base2_model.layers:
 base3_model = Sequential()
 base3_model.add(ResNet50(include_top=False, weights=None, pooling='max'))
 base3_model.add(Dense(4, activation='sigmoid'))
+base3_model.add(Activation("relu"))
 
 
-# In[ ]:
+# In[129]:
 
 
-resnet_history3 = base3_model.fit(train_generator, validation_data = validation_generator, steps_per_epoch = 100, epochs = 10)
+base3_model.compile(optimizer = tf.keras.optimizers.SGD(lr=0.0001), loss = 'binary_crossentropy', metrics = ['acc'])
 
 
-# In[ ]:
+# In[130]:
+
+
+resnet_history3 = base3_model.fit(train_generator, validation_data = validation_generator, steps_per_epoch = 50, epochs = 20)
+
+
+# In[131]:
 
 
 base3_model.save('ResNet50-Try3.h5')
 print("Saved model")
 
 
-# In[ ]:
+# In[165]:
 
 
-# save model to file
-pickle.dump(base3_model, open('base3_model.pickle.dat', 'wb'))
+resnet_history3 = load_model('ResNet50-Try3.h5')
+print("Loaded model")
 
 
-# In[ ]:
+# In[133]:
 
 
 from sklearn.metrics import accuracy_score
@@ -516,7 +624,7 @@ val_acc_values3 = history_dict3['val_acc']
 epochs3 = range(1, len(history_dict3['acc']) + 1)
 
 
-# In[ ]:
+# In[134]:
 
 
 # Plot the training/validation loss
@@ -529,7 +637,7 @@ plt.legend()
 plt.show()
 
 
-# In[ ]:
+# In[135]:
 
 
 # Plot the training/validation accuracy
@@ -542,7 +650,7 @@ plt.legend()
 plt.show()
 
 
-# In[ ]:
+# In[136]:
 
 
 # Evaluate the test accuracy and test loss of the model
@@ -550,11 +658,11 @@ test_loss3, test_acc3 = base3_model.evaluate_generator(validation_generator)
 print('Model testing accuracy/testing loss:', test_acc3, " ", test_loss3)
 
 
-# In[ ]:
+# In[166]:
 
 
 # Test Data Directory and Looping Over Images
-test_img_dir = r"/Users/SahilSachdev/Downloads/MLPA-Final/data/Test/test/" 
+test_img_dir = r"/Users/SahilSachdev/Downloads/MLPA-Final/data/Test/test/Groups/*" 
 data_path = os.path.join(test_img_dir, '*g')
 files = glob.glob(data_path)
 
@@ -565,6 +673,7 @@ results = []
 for f1 in files:
     img = image.load_img(f1, target_size = (225, 225))
     img = image.img_to_array(img)
+    img = np.expand_dims(img, axis = 0)
     img = preprocess_input(img)
     result = base3_model.predict(img)
     r = np.argmax(result, axis=1)
@@ -573,19 +682,45 @@ for f1 in files:
 print(results)
 
 
-# In[ ]:
+# In[142]:
+
+
+true_classes = validation_generator.classes
+class_indices = train_generator.class_indices
+class_indices = dict((v,k) for k,v in class_indices.items())
+
+resnet_preds = base3_model.predict(validation_generator)
+resnet_pred_classes = np.argmax(resnet_preds, axis=1)
+
+resnet_acc = accuracy_score(true_classes, resnet_pred_classes)
+print("ResNet50 Model Accuracy without Fine-Tuning: {:.2f}%".format(resnet_acc * 100))
+
+print(true_classes)
+print(resnet_pred_classes)
+
+accuracy = accuracy_score(true_classes, resnet_pred_classes)
+print('\n\nThe accuracy score is:', accuracy)
+
+# Summarize the fit of the model on the test data
+print('\n Classification report of the test data: \n')
+print(metrics.classification_report(true_classes, resnet_pred_classes))
+print('\n Confusion matrix of the test data: \n')
+print(metrics.confusion_matrix(true_classes, resnet_pred_classes, normalize='true').round(3))
+
+
+# In[162]:
 
 
 base3_model.summary()
 
 
-# In[ ]:
+# In[163]:
 
 
 base2_model.summary()
 
 
-# In[ ]:
+# In[164]:
 
 
 base_model.summary()
